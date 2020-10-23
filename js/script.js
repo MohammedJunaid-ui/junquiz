98ec940626759ffbd9a14148a2d0d67b8bd49673
Mohammed Junaid Ap <junaidapmohammed@gmail.com>
2020-10-23 07:04:45 -0700

    edit

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ const time_line = document.querySelector("header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 
+
 // if malayalam button clicked
 malayalam_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show info box
