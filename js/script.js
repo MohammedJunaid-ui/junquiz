1078ab6e06a7512ebf3678106fcf6ae710b7b37b
Mohammed Junaid Ap <junaidapmohammed@gmail.com>
2020-10-23 10:23:08 -0700

    pwa

--- js/script.js
+++ js/script.js
@@ -12,6 +12,27 @@ const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 
 
+// On install - caching the application shell
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('sw-cache').then(function(cache) {
+        // cache any static files that make up the application shell
+        return cache.add('index.html');
+      })
+    );
+  });
+  
+  // On network request
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      // Try the cache
+      caches.match(event.request).then(function(response) {
+        //If response found return it, else fetch again
+        return response || fetch(event.request);
+      })
+    );
+  });
+
 // if malayalam button clicked
 malayalam_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show info box
